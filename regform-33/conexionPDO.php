40d25d76973d5a9a2cc2efffebcb9facf88b683a
Héctor González Mojarro <t0rhec@outlook.com>
2019-11-08 08:40:52 -0600

    cosas2

--- regform-33/conexionPDO.php
+++ regform-33/conexionPDO.php
@@ -5,7 +5,7 @@
 
 		public static function conectar(){
 			$pdo_options[PDO::ATTR_ERRMODE]=PDO::ERRMODE_EXCEPTION;
-			self::$conexion=new PDO('mysql:host=localhost;dbname=Vehicular','root','admin',$pdo_options);
+			self::$conexion=new PDO('mysql:host=localhost;dbname=Vehicular','root','',$pdo_options);
 			return self::$conexion;
 		}
 	}
