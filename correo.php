7f3c01a43b6c4763c7c7a29ad75ff058fc3e3024
Héctor González Mojarro <t0rhec@outlook.com>
2019-11-09 13:37:02 -0600

    Correccion envio de correos

--- correo.php
+++ correo.php
@@ -1,11 +1,45 @@
 <?php
-	$destino = "PVehicular007@gmail.com";
-	$nombre = $_POST["nombre"];
+
 	$correo = $_POST["correo"];
 	$tema = $_POST["tema"];
 	$mensaje = $_POST["mensaje"];
-	$contenido = "Nombre: ".$nombre ."\nCorreo: ".$correo ."\nTema: " .$tema ."\nMensaje: " .$mensaje ;
-	mail($destino, $tema, $contenido);
+
+	$url = 'https://api.sendgrid.com/';
+	$user = 'azure_893e8997c62517bf176b1261749ff855@azure.com';
+	$pass = 'Messier3203.';
+
+	$params = array(
+	'api_user' => $user,
+	'api_key' => $pass,
+	'to' => 'pvehicular007@gmail.com',
+	'subject' => $tema,
+	'html' => $mensaje,
+	'text' => $mensaje,
+	'from' => $correo,
+	);
+
+	$request = $url.'api/mail.send.json';
+
+	// Generate curl request
+	$session = curl_init($request);
+
+	// Tell curl to use HTTP POST
+	curl_setopt ($session, CURLOPT_POST, true);
+
+	// Tell curl that this is the body of the POST
+	curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
+
+	// Tell curl not to return headers, but do return the response
+	curl_setopt($session, CURLOPT_HEADER, false);
+	curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+
+	// obtain response
+	$response = curl_exec($session);
+	curl_close($session);
+
+	// print everything out
+	// print_r($response);
+
 	header("Location: contact.html");
 
 ?>
